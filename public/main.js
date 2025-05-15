26e46ea8d1b1fb5d2ec2ad62d572af89b2adf14f
Azure DevOps Pipeline <azuredevops@example.com>
2025-05-15 10:26:23 +0000

    Update documentation site from source repository

---
+++ public/main.js
@@ -0,0 +1,9 @@
+// XMPro Custom JavaScript for DocFX
+
+export default {
+  start: () => {
+    // Startup script goes here
+    // console.log("xmpro-template/public/main.js loaded");
+    
+  },
+}
